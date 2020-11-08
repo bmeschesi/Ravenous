7b34da3c74ce6e9716259062a498b1dce9b490b3
Bruno Meschesi <72936169+bmeschesi@users.noreply.github.com>
2020-11-08 12:20:16 +0000

    business css impoerted

--- public/components/Business/Business.js
+++ public/components/Business/Business.js
@@ -1,5 +1,5 @@
-import react from 'react';
 import React from 'react';
+import './Business.css';
 
 const business = {
     imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
