3684d33c7d5ad9980df6bc879a06f33117c3782d
Bruno Meschesi <72936169+bmeschesi@users.noreply.github.com>
2020-11-08 12:23:11 +0000

    correction on code js (business.rating)

--- public/components/Business/Business.js
+++ public/components/Business/Business.js
@@ -13,7 +13,7 @@ const business = {
     reviewCount: 90
 };
 
-class Business extends react.Component {
+class Business extends React.Component {
     render() {
         return (
             <div className="Business">
@@ -29,7 +29,7 @@ class Business extends react.Component {
               </div>
               <div className="Business-reviews">
                 <h3>{business.category}</h3>
-                <h3 className="rating">4.5 stars}</h3>
+                <h3 className="rating">{business.rating} stars</h3>
                 <p>{business.reviewCount} reviews</p>
               </div>
             </div>
