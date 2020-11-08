0d17694377bf2f77fd6f7b3f25715b955b36df7e
Bruno Meschesi <72936169+bmeschesi@users.noreply.github.com>
2020-11-08 12:15:36 +0000

    update business js

--- public/components/Business/Business.js
+++ public/components/Business/Business.js
@@ -1,4 +1,7 @@
-const business {
+import react from 'react';
+import React from 'react';
+
+const business = {
     imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
     name: 'MarginOtto Pizzeria',
     address: '1010 Paddington Way',
@@ -9,3 +12,28 @@ const business {
     rating: 4.5,
     reviewCount: 90
 };
+
+class Business extends react.Component {
+    render() {
+        return (
+            <div className="Business">
+            <div className="image-container">
+              <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+            </div>
+            <h2>{business.name}</h2>
+            <div className="Business-information">
+              <div className="Business-address">
+                <p>{business.address}</p>
+                <p>{business.city}</p>
+                <p>{business.state + ' ' + business.zipCode}</p>
+              </div>
+              <div className="Business-reviews">
+                <h3>{business.category}</h3>
+                <h3 className="rating">4.5 stars}</h3>
+                <p>{business.reviewCount} reviews</p>
+              </div>
+            </div>
+          </div> 
+        )
+    }
+}
