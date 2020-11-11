afc9d2dabd8b1fa41613dd52f5a78ec15d55fcc7
Bruno Meschesi <72936169+bmeschesi@users.noreply.github.com>
2020-11-11 16:07:29 +0000

    last update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,11 @@
-import logo from '../../logo.svg';
+import React from 'react';
 import './App.css';
+import logo from '../../logo.svg';
+
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+
+
 
 function App() {
   return (
